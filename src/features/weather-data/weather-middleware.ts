d7c1d464ad1e1a7944a9a395adce720074af29d1
Addie Cohen <addie@swimm.io>
2022-02-10 11:50:28 +0200

    for flows

--- src/features/weather-data/weather-middleware.ts
+++ src/features/weather-data/weather-middleware.ts
@@ -1,19 +1,6 @@
 import {fetchPosition, fetchWeather} from './weather-api';
 import {setMessage, setPosition, setWeatherForecast} from './weather-data-slice';
 
-export const getPositionMiddleware = (storeApi: any) => (next: Function) => async (action: any) => {
-    if (action.type === 'weatherData/setCityName') {
-        const response = await fetchPosition(action.payload) as any
-        if (response.cod !== 200) {
-            storeApi.dispatch(setMessage(response.message));
-        } else {
-            storeApi.dispatch(setMessage(''));
-            storeApi.dispatch(setPosition({lat: response.coord.lat, long: response.coord.lon}));
-        }
-    }
-
-    return next(action);
-}
 
 export const getWeatherMiddleware = (storeApi: any) => (next: Function) => async (action: any) => {
     if (action.type === 'weatherData/setPosition') {
